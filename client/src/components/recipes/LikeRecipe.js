a559808af5be68de82ce599c7270b554a7fbc72c
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-04-19 22:18:06 +0200

    Add client-side logic to implement toggle like button

--- client/src/components/recipes/LikeRecipe.js
+++ client/src/components/recipes/LikeRecipe.js
@@ -7,31 +7,51 @@ import { LIKE_RECIPE } from '../../queries/index';
 class LikeRecipe extends Component {
 
     state = {
+        liked: false,
         username: ''
     }
 
     componentDidMount() {
         if (this.props.session.getCurrentUser) {
-            const { username } = this.props.session.getCurrentUser;
+            const { username, favourites } = this.props.session.getCurrentUser;
+            const { _id } = this.props;
+            const prevLiked = favourites.findIndex(favourite => favourite._id === _id) > -1;
             this.setState({
+                liked: prevLiked,
                 username
             }); 
         }
     }
 
-    handleLike = (likeRecipe) => {
-        likeRecipe().then(({ data }) => {
-            console.log(data);
+    handleClick = (likeRecipe) => {
+        this.setState(prevState => ({
+            liked: !prevState.liked
+        }), () => {
+            this.handleLike(likeRecipe);
         });
     }
 
+    handleLike = (likeRecipe) => {
+        if (this.state.liked) {
+            likeRecipe().then(async ({ data }) => {
+                console.log(data);
+                await this.props.refetch();
+            });
+        } else {
+            // unlike 
+            console.log('unlike');
+        }
+    }
+
     render() {
-        const { username } = this.state;
+        const { liked, username } = this.state;
         const { _id } = this.props;
         return (
             <Mutation mutation={LIKE_RECIPE} variables={{ _id, username }}>
                 {likeRecipe => (
-                    (username && <button onClick={() => this.handleLike(likeRecipe)}>Like</button>)
+                    (username && <button onClick={() => this.handleClick(likeRecipe)}>
+                        {liked ? 'Liked' : 'Like'}
+                    </button>)
                 )}
             </Mutation>
         )
